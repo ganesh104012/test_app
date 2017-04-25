5da95676e8cac93d150ab930c662b3098b2d43cb
ganesh104012 <ganesh104012@gmail.com>
2017-04-25 06:43:47 +0530

    First commit

---
+++ www/js/services.js
@@ -0,0 +1,52 @@
+angular.module('starter.services', [])
+
+.service('LoginService', function($q,$cookieStore,$rootScope,$timeout) {
+	return {
+		loginUser: function(usr,pwd){
+			var deferred=$q.defer();
+			var promise=deferred.promise;
+			if(usr!=""&&pwd!="")
+			{
+				this.SetCredentials(usr,pwd);
+				$timeout(function(){
+				deferred.resolve('Welcome '+usr+'!');
+				},2000);
+			}
+			else
+			{
+				deferred.reject('Wrong Credentials!');
+			}
+			promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+			}
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+			}
+			return promise;
+		},
+		SetCredentials : function (username, password) {
+            // var authdata = Base64.encode(username + ':' + password);
+ 
+            $rootScope.globals = {
+                currentUser: {
+                    username: username
+                }
+            };
+ 
+            // $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+            $cookieStore.put('globals', $rootScope.globals);
+        },
+ 
+        ClearCredentials : function () {
+            $rootScope.globals = {};
+            $cookieStore.remove('globals');
+            // $http.defaults.headers.common.Authorization = 'Basic ';
+        },
+		isLogin: function(){
+			
+			return true;
+		}
+	}
+});
